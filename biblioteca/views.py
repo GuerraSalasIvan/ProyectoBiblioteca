cf49edc249f9cfbd0aa870ce1050b6eede1b4e6c
Ivan Guerra UbuntuClase <guerrasalasivan@gmail.com>
2023-10-18 13:28:24 +0200

    realizado el primer ejercicio de urls, view y temp

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def indice(request):
+    return render(request, 'index.html', {})
+
+def listar_libros(request):
+    #libros = Libro.objects.select_related('biblioteca').prefetch_related('autores')
+    
+    libros = Libro.objects.all
+    
+    return render(request, 'libro_listar.html', {'libros_mostrar':libros})
